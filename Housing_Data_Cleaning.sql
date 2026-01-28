aa450b9fc6a66c487610ed29ecc7b2ba277d51b3
Dana Alturaif <114310729+DanaAlturaif@users.noreply.github.com>
2026-01-28 22:48:04 +0300

    Add files via upload

---
+++ Housing_Data_Cleaning.sql
@@ -0,0 +1,187 @@
+/*
+
+Cleaning data in SQL Queries 
+
+*/
+
+Select * 
+From PortfolioProject.dbo.NashHousing
+
+---------------------------------------------------------
+
+--Standardize data format 
+
+Select SaleDateConverted , CONVERT(Date,SaleDate)
+From PortfolioProject.dbo.NashHousing
+
+--It did not update properly, so this was used 
+
+ALTER TABLE NashHousing
+Add SaleDateConverted Date;
+
+Update NashHousing
+SET SaleDateConverted = CONVERT(Date,SaleDate) 
+
+---------------------------------------------------------
+
+--Populate property address data 
+
+Select * 
+From PortfolioProject.dbo.NashHousing
+--Where PropertyAddress is null
+order by ParcelID
+
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortfolioProject.dbo.NashHousing a
+JOIN PortfolioProject.dbo.NashHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID] <> b.[UniqueID]
+Where a.PropertyAddress is null 
+
+Update a 
+SET PropertyAddress = ISNULL (a.PropertyAddress, b.PropertyAddress)
+FROM PortfolioProject.dbo.NashHousing a
+JOIN PortfolioProject.dbo.NashHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID] <> b.[UniqueID]
+Where a.PropertyAddress is null 
+
+---------------------------------------------------------
+
+--Breaking out address into individual columns (address, city, state)
+
+Select PropertyAddress 
+FROM PortfolioProject.dbo.NashHousing
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as Address
+
+From PortfolioProject.dbo.NashHousing
+
+
+
+ALTER TABLE NashHousing
+Add PropertySplitAddress Nvarchar(255);
+
+Update NashHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
+
+
+
+ALTER TABLE NashHousing
+Add PropertySplitCity Nvarchar(255);
+
+Update NashHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress))
+
+
+
+Select *
+From PortfolioProject.dbo.NashHousing
+
+
+
+Select OwnerAddress
+From PortfolioProject.dbo.NashHousing
+
+
+Select
+PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+From PortfolioProject.dbo.NashHousing
+
+
+ALTER TABLE NashHousing
+Add OwnerSplitAddress Nvarchar(255);
+
+Update NashHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+
+
+ALTER TABLE NashHousing
+Add OwnerSplitCity Nvarchar(255);
+
+Update NashHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+
+
+
+ALTER TABLE NashHousing
+Add OwnerSplitState Nvarchar(255);
+
+Update NashHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+
+
+
+Select *
+From PortfolioProject.dbo.NashHousing
+
+
+---------------------------------------------------------
+
+--Change Y and N to Yes and No in 'Sold as Vacant" field 
+
+Select Distinct(SoldAsVacant), Count(SoldAsVacant)
+From PortfolioProject.dbo.NashHousing
+Group by SoldAsVacant
+order by 2
+
+
+Select SoldAsVacant
+, CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+From PortfolioProject.dbo.NashHousing
+
+
+Update NashHousing
+SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+
+---------------------------------------------------------
+
+--Remove duplicates 
+
+WITH RowNumCTE AS(
+Select *,
+	ROW_NUMBER() OVER (
+	PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY
+					UniqueID
+					) row_num
+
+From PortfolioProject.dbo.NashHousing
+--order by ParcelID
+)
+Select *
+From RowNumCTE
+Where row_num > 1
+Order by PropertyAddress
+
+
+
+Select *
+From PortfolioProject.dbo.NashHousing
+
+
+---------------------------------------------------------
+
+--Delete unused columns 
+
+Select *
+From PortfolioProject.dbo.NashHousing
+
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
+
